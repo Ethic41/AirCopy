7f5265bdf27161c8247fe517d0e871bb15ab4c5d
ethic41 <dahirmuhammad3@gmail.com>
2019-06-15 22:45:49 +0100

    updated project structure

---
+++ src/main/python/core/__init__.py
@@ -0,0 +1,6 @@
+# Author: Dahir Muhammad Dahir
+# Date: 15-June-2019 10:39 PM
+# About: I will tell you later
+ 
+ 
+ 
---
+++ src/main/python/rough/__init__.py
@@ -0,0 +1,6 @@
+# Author: Dahir Muhammad Dahir
+# Date: 15-June-2019 10:44 PM
+# About: I will tell you later
+ 
+ 
+ 
